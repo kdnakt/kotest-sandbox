a1062c7b3f347f3007b30a4cb4ed2ee56f98b713
kdnakt <a.kid.1985@gmail.com>
2022-02-11 15:10:46 +0900

    Add startWith test

--- lib/src/test/kotlin/kotest/sandbox/MyTests.kt
+++ lib/src/test/kotlin/kotest/sandbox/MyTests.kt
@@ -1,10 +1,15 @@
 package kotest.sandbox
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.startWith
 
 class MyTests : StringSpec({
     "length should return size of string" {
         "hello world".length shouldBe 11
+    }
+    "startsWith should test for a prefix" {
+        "sandbox" should startWith("sand")
     }
 })
