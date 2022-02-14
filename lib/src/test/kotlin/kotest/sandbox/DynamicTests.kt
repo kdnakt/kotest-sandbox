5a5625c854f8d7625b545287dcdf872d78133894
kdnakt <a.kid.1985@gmail.com>
2022-02-15 00:29:57 +0900

    Add dynamic tests

---
+++ lib/src/test/kotlin/kotest/sandbox/DynamicTests.kt
@@ -0,0 +1,17 @@
+package kotest.sandbox
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.string.shouldHaveLength
+
+class DynamicTests : FunSpec({
+
+    listOf(
+            "sam",
+            "pam",
+            "tim",
+    ).forEach {
+        test("$it should be a three letter name") {
+            it.shouldHaveLength(3)
+        }
+    }
+})
