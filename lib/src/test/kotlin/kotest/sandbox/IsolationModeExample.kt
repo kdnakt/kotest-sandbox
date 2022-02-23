ae949aff41ab7b66d6c2297896f6ac3a6ec43744
kdnakt <a.kid.1985@gmail.com>
2022-02-23 17:30:16 +0900

    Isolate per test, per leaf

---
+++ lib/src/test/kotlin/kotest/sandbox/IsolationModeExample.kt
@@ -0,0 +1,110 @@
+package kotest.sandbox
+
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.shouldBe
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+
+class IsolationModeWithDSL : WordSpec({
+    isolationMode = IsolationMode.SingleInstance
+    // tests here
+    "Hello" When {
+        "asked for length" should {
+            "return 5" {
+                "Hello".length shouldBe 5
+            }
+        }
+        "appended to Bob" should {
+            "return Hello Bob" {
+                "Hello " + "Bob" shouldBe "Hello Bob"
+            }
+        }
+    }
+})
+
+
+class IsolationModeWithOverrideFun : WordSpec() {
+    override fun isolationMode() = IsolationMode.SingleInstance
+
+    init {
+        // tests here
+        "Hello" When {
+            "asked for length" should {
+                "return 5" {
+                    "Hello".length shouldBe 5
+                }
+            }
+            "appended to Bob" should {
+                "return Hello Bob" {
+                    "Hello " + "Bob" shouldBe "Hello Bob"
+                }
+            }
+        }
+    }
+}
+
+class SingleInstanceExample : WordSpec({
+    // default isolation mode is SingleInstance
+    val id = UUID.randomUUID()
+    "a" should {
+        println(id)
+        "b" {
+            println(id)
+        }
+        "c" {
+            println(id)
+        }
+    }
+})
+
+class InstancePerTestExample : WordSpec() {
+    override fun isolationMode(): IsolationMode = IsolationMode.InstancePerTest
+    val id = UUID.randomUUID()
+    init {
+        "a" should {
+            println("Hello $id")
+            "b" {
+                println("From $id")
+            }
+            "c" {
+                println("Sam $id")
+            }
+        }
+    }
+}
+
+class InstancePerTestWithCounter : WordSpec() {
+
+    override fun isolationMode(): IsolationMode = IsolationMode.InstancePerTest
+
+    val counter = AtomicInteger(0)
+
+    init {
+        "a" should {
+            println("a=" + counter.getAndIncrement())
+            "b" {
+                println("b=" + counter.getAndIncrement())
+            }
+            "c" {
+                println("c=" + counter.getAndIncrement())
+            }
+        }
+    }
+}
+
+class InstancePerLeafExample : WordSpec() {
+    override fun isolationMode(): IsolationMode = IsolationMode.InstancePerLeaf
+    val id = UUID.randomUUID()
+    init {
+        "a" should {
+            println("Hello $id")
+            "b" {
+                println("From $id")
+            }
+            "c" {
+                println("Sam $id")
+            }
+        }
+    }
+}
