75fd05c73e57fcde90bdd60142ed16a3618a08de
kdnakt <a.kid.1985@gmail.com>
2022-03-09 09:37:35 +0900

    Data test name with map

--- lib/src/test/kotlin/kotest/sandbox/SimpleDataDrivenTest.kt
+++ lib/src/test/kotlin/kotest/sandbox/SimpleDataDrivenTest.kt
@@ -29,4 +29,17 @@ class SimpleDataDrivenTest : FunSpec({
             isPythagTriple(a, b, c) shouldBe false
         }
     }
+
+    context("Pythag triples tests with map name") {
+        withData(
+            mapOf(
+                "3, 4, 5" to PythagTriple(3, 4, 5),
+                "6, 8, 10" to PythagTriple(6, 8, 10),
+                "8, 15, 17" to PythagTriple(8, 15, 17),
+                "7, 24, 25" to PythagTriple(7, 24, 25)
+            )
+        ) { (a, b, c) ->
+            a * a + b * b shouldBe c * c
+        }
+    }
 })
