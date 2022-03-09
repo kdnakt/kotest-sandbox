523e85eb4e7813f3f3d771fcf6785d7e5fc71a89
kdnakt <a.kid.1985@gmail.com>
2022-03-09 21:51:15 +0900

    test name function

--- lib/src/test/kotlin/kotest/sandbox/SimpleDataDrivenTest.kt
+++ lib/src/test/kotlin/kotest/sandbox/SimpleDataDrivenTest.kt
@@ -42,4 +42,16 @@ class SimpleDataDrivenTest : FunSpec({
             a * a + b * b shouldBe c * c
         }
     }
+
+    context("Pythag triples tests with NameFn") {
+        withData<PythagTriple>(
+            nameFn = { "${it.a}__${it.b}__${it.c}" },
+            PythagTriple(3, 4, 5),
+            PythagTriple(6, 8, 10),
+            PythagTriple(8, 15, 17),
+            PythagTriple(7, 24, 25)
+        ) { (a, b, c) ->
+            a * a + b * b shouldBe c * c
+        }
+    }
 })
