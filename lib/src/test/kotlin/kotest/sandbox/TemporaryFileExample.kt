2ff7ced8cd3375ec093ca5d1d8ce1bc28bdc3698
kdnakt <a.kid.1985@gmail.com>
2022-03-28 09:49:10 +0900

    test with tempdir()

--- lib/src/test/kotlin/kotest/sandbox/TemporaryFileExample.kt
+++ lib/src/test/kotlin/kotest/sandbox/TemporaryFileExample.kt
@@ -1,12 +1,14 @@
 package kotest.sandbox
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.engine.spec.tempdir
 import io.kotest.engine.spec.tempfile
 import io.kotest.matchers.shouldBe
 
 class TemporaryFileExample : FunSpec({
 
     val file = tempfile()
+    val dir = tempdir()
 
     test("a temporary file dependent test") {
         file.writeText("this is a test")
@@ -15,5 +17,9 @@ class TemporaryFileExample : FunSpec({
             println("text is: $text")
             text shouldBe "this is a test"
         }
+    }
+
+    test("a temporary dir dependent test") {
+        dir.isDirectory shouldBe true
     }
 })
