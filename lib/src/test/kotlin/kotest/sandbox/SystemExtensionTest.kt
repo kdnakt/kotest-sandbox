5de89d865444eaf2791d7cef0c190ead6242a44c
kdnakt <a.kid.1985@gmail.com>
2022-04-21 02:51:19 +0900

    use multiple environment variables

--- lib/src/test/kotlin/kotest/sandbox/SystemExtensionTest.kt
+++ lib/src/test/kotlin/kotest/sandbox/SystemExtensionTest.kt
@@ -2,6 +2,7 @@ package kotest.sandbox
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.extensions.system.withEnvironment
+import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.shouldBe
 
 class SystemExtensionTest: StringSpec({
@@ -12,4 +13,15 @@ class SystemExtensionTest: StringSpec({
             value shouldBe "BarValue" // System environment overridden!
         }
     }
+
+    "withEnvironment(mapOf())" {
+        withEnvironment(mapOf("FooKey" to "BarValue", "BarKey" to "FooValue")) {
+            val val1 = System.getenv("FooKey")
+            println(val1)
+            val val2 = System.getenv("BarKey")
+            println(val2)
+            val1.length shouldBeExactly val2.length
+        }
+    }
+
 })
